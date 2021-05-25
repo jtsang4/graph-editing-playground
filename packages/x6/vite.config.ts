a8750059f16bed3d1aabf83acdd77f53e5dc69ac
wtzeng1 <wtzeng1@gmail.com>
2021-05-25 14:15:42 +0800

    chore: fix vite and ts config

--- packages/x6/vite.config.ts
+++ packages/x6/vite.config.ts
@@ -3,7 +3,8 @@ import defaultConfig from '../../vite.base.config';
 
 export default defineConfig({
   ...defaultConfig,
+  root: __dirname,
   optimizeDeps: {
-    entries: '../../index.html'
+    entries: 'index.html'
   },
 });
