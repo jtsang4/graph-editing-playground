bbb9048b0e4517449a6889a5f0cc6573a1a1a6b8
wtzeng1 <wtzeng1@gmail.com>
2021-05-25 14:53:02 +0800

    chore: optimize vite cofig

--- packages/x6/vite.config.ts
+++ packages/x6/vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import defaultConfig from '../../vite.base.config';
 
 export default defineConfig({
   ...defaultConfig,
-  root: __dirname,
-  optimizeDeps: {
-    entries: 'index.html'
-  },
 });
--- vite.base.config.ts
+++ vite.base.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import WindiCSS from "vite-plugin-windicss";
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 2333,
   },
-  plugins: [
-    reactRefresh(),
-    WindiCSS(),
-  ],
+  plugins: [reactRefresh(), WindiCSS()],
+  optimizeDeps: {
+    entries: 'index.html',
+  },
 });
