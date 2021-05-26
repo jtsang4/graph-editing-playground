fc511ca1475420be60561e88991d59cea461f201
wtzeng1 <wtzeng1@gmail.com>
2021-05-26 11:26:18 +0800

    feat: complete jointjs demo

--- packages/jointjs/src/app.tsx
+++ packages/jointjs/src/app.tsx
@@ -20,6 +20,7 @@ export const App: React.FC<{}> = () => {
       drawGrid: true,
     } as dia.Paper.Options);
 
+    // 这里看文档只知道可以用 shapes.dev.Model 来设置 port，但使用这个会有问题，无法不带标签地渲染上下结构的 port，现在的用法是在 stackovrflow 里找到的
     const rect = new shapes.standard.Rectangle({
       size: {
         width: 120,
@@ -28,6 +29,17 @@ export const App: React.FC<{}> = () => {
       position: {
         x: 520,
         y: 50,
+      },
+      ports: {
+        groups: {
+          in: {
+            position: { name: 'top' },
+          },
+          out: {
+            position: { name: 'bottom' },
+          },
+        },
+        items: [{id: '1', group: 'in'}, { id: '2', group: 'out' }]
       },
     });
     rect.attr({
