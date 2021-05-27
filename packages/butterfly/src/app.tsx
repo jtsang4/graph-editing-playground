c983a25dda01bfd88344584e51923a6946d5229d
wtzeng1 <wtzeng1@gmail.com>
2021-05-27 11:45:11 +0800

    feat: complete butterfly demo

--- packages/butterfly/src/app.tsx
+++ packages/butterfly/src/app.tsx
@@ -1,84 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactButterfly from 'butterfly-react';
+import produce from 'immer';
+import { NodeComponent } from './component/node';
 import styles from './app.module.scss';
 
 const endpoints = [
   {
-    id: 'right',
-    orientation: [1, 0],
-    pos: [0, 0.5],
+    id: 'top',
+    orientation: [0, -1],
+    pos: [0.5, 0],
   },
   {
-    id: 'left',
-    orientation: [-1, 0],
-    pos: [0, 0.5],
+    id: 'bottom',
+    orientation: [0, 1],
+    pos: [0.5, 0],
   },
 ];
 
-const data = {
-  groups: [
-    {
-      id: '1',
-      left: 10,
-      top: 100,
-      render() {
-        return (
-          <div
-            style={{
-              width: '250px',
-              textAlign: 'center',
-              backgroundColor: 'blanchedalmond',
-            }}
-          >
-            测试group
-          </div>
-        );
-      },
-    },
-  ],
-  nodes: [
-    {
-      id: '1',
-      group: '1',
-      endpoints: endpoints,
-      render() {
-        return <div>测试节点1</div>;
-      },
-    },
-    {
-      id: '2',
-      top: 25,
-      left: 300,
-      endpoints: endpoints,
-      render() {
-        return <div>测试节点2</div>;
-      },
-    },
-    {
-      id: '3',
-      top: 25,
-      left: 600,
-      endpoints: endpoints,
-      render() {
-        return <div>测试节点3</div>;
-      },
-    },
-  ],
-  edges: [
-    {
-      id: '1-2',
-      sourceNode: '1',
-      targetNode: '2',
-      source: 'right',
-      target: 'left',
-      labelRender: () => {
-        return '测试label';
-      },
-    },
-  ],
-};
-
 export const App: React.FC<{}> = () => {
+  const [data, setData] = useState({
+    nodes: [
+      {
+        id: '1',
+        top: 50,
+        left: 520,
+        endpoints: endpoints,
+        render() {
+          return <NodeComponent label="Hello" />;
+        },
+      },
+      {
+        id: '2',
+        top: 260,
+        left: 720,
+        endpoints: endpoints,
+        render() {
+          return <NodeComponent label="World!" />;
+        },
+      },
+    ],
+    edges: [
+      {
+        id: '1-2',
+        sourceNode: '1',
+        targetNode: '2',
+        source: 'bottom',
+        target: 'top',
+      },
+    ],
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('更新画布');
+      setData(produce(prevData => {
+        prevData.nodes[0].render = () => {
+          return <NodeComponent label="Hello," />;
+        };
+        prevData.nodes[1].render = () => {
+          return <NodeComponent label="World." />;
+        };
+      }));
+    }, 2000);
+  }, []);
+
   return (
     <div className={styles.app}>
       <ReactButterfly {...data} />
